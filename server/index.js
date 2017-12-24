e8328bb099b39dfbc8e2cdffd4ac4090962db342
Simone Boscolo Berto <simone.boscoloberto@it.ibm.com>
2017-12-24 19:51:40 +0100

    Base structure

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/temperature', (req, res) => {
+    res.send('24 °C');
+});
+
+app.get('/humidity', (req, res) => {
+    res.send('48%');
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
