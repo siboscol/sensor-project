cc0aed361c3d85b88487b3730406965f0abaf4fa
Simone Boscolo Berto <simone.boscoloberto@it.ibm.com>
2017-12-25 04:34:58 +0100

    Adding sensor libray

---
+++ server/obtain-readings.js
@@ -0,0 +1,12 @@
+// Import the sensor library
+const sensor = require(node-dht-sensor);
+
+// The first argument is the sensor number. In this case 22 represents DHT22 sensor
+// The second argument is the pin number to read from, for this example pin to pin 2
+sensor.read(22, 2, (err, temperature, humidity) => {
+    // After reading the sensor, we get the temperature and humidity reading
+    if(!err) {
+        // If there is no error, log the readings to the console
+        console.log('temp:' + temperature.toFixed(1) + '°C, ' + 'humidity: ' + humidity.toFixed(1) + '%');
+    }
+});
