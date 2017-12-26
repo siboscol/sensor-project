230fcd9f280df4f640741b4783f12826d224f680
Simone Boscolo Berto <simone.boscoloberto@it.ibm.com>
2017-12-26 19:35:12 +0100

    Setting temperature to index.html

--- public/script.js
+++ public/script.js
@@ -1,5 +1,12 @@
+/* The fetch API uses a promise based sintax.
+It may look a little weird if you are seeing it for the first time, but it's an improvement over callbacks. */
+
+/* First, we instanciate the first promise, which call the API a /temperature of our server. */
 fetch('/temperature').then(results => {
+    /* results.text() returns another promise, which resolves to the text response we receive from the API.  */
     return results.text();
 }).then(text => {
-    console.log('Temperature: ' + text);
+    /* This "text" variable is the response that the server gives us. */
+    /* Get the 'p' element as a variable, and set its inner HTML to the response we received from the server. */
+    const temperatureDisplay = document.getElementById('temperature-display').innerHTML = text;
 });
