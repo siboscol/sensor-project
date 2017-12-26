e200fb9070a58559a0a50418862408626a5436f5
Simone Boscolo Berto <simone.boscoloberto@it.ibm.com>
2017-12-26 21:46:52 +0100

    Fixing json

--- public/script.js
+++ public/script.js
@@ -4,12 +4,7 @@ const fetchTemperature = () => {
 
     /* First, we instanciate the first promise, which call the API a /temperature of our server. */
     fetch('/temperature').then(results => {
-        /* results.text() returns another promise, which resolves to the text response we receive from the API.  */
-        return results.text();
-    }).then(text => {
-        /* This "text" variable is the response that the server gives us. */
-        /* Get the 'p' element as a variable, and set its inner HTML to the response we received from the server. */
-        const temperatureDisplay = document.getElementById('temperature-display').innerHTML = text;
+        const temperatureDisplay = document.getElementById('temperature-display').innerHTML = results;
     });
 }
 
