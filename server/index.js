0dcc5d0951153eea7108b633b363a1c179ef097f
Simone Boscolo Berto <simone.boscoloberto@it.ibm.com>
2017-12-26 17:39:56 +0100

    Serving statis files

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const getCachedSensorReadings = require('./get-cached-sensor-readings');
 
+/* Here, we are introduced to express middleware.
+Middleware is a fancy word to describe a set of actions that have to take place before the request handler.
+In the below statement, we use the express static middleware, and bind it to the /public route.*/
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.get('/temperature', (req, res) => {
     res.send(getCachedSensorReadings.getTemperature().toFixed(1) + '°C');
 });
@@ -11,6 +17,7 @@ app.get('/humidity', (req, res) => {
     res.send(getCachedSensorReadings.getHumidity().toFixed(1) + '%');
 });
 
+// Opening the server on port 3000
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
