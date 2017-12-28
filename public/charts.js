76187b1cc381a7a5c56d29ef31ba85a9a96a3bbc
Simone Boscolo Berto <simone.boscoloberto@it.ibm.com>
2017-12-28 23:23:46 +0100

    Setting dynamic data to temperature chart

---
+++ public/charts.js
@@ -0,0 +1,66 @@
+/* Get the contex of the temperature canvas element. */
+const temperatureCanvasCtX = document.getElementById('temperature-chart').getContext('2d');
+/* Create a new chart on the context we just instanciated. */
+export const temperatureChart = new Chart(temperatureCanvasCtX,
+{
+    /* We are going to show the ongoing temperature as a line chart. */
+    type: 'line',
+    data: {
+        /* For our actual data, we will not have any reading initially.
+        The labels, which will form our x-axis, are suppose to represent the time at which each reading was taken.
+        Finally, we add the dataset, whose data is an array of temperature values.
+        The background color is set to the same value as earlier display, with some added transparency (which is why the 'rgba' representation is used) */
+        labels: ['10:30', '10.31', '10.32', '10.33'],
+        datasets: [{
+            data: [12, 19, 23, 17],
+            backgroundColor: 'rgba(255, 205, 210, 0.5)'
+        }]
+    },
+    options: {
+        /* There is no need for a legend since there is only one dataset plotted.
+        The 'responsive' and 'mantainAspectRadio' options are set so that the chart takes the width and height of the canvas, and does not set it on its own. */
+        legend: {
+            display: false
+        },
+        responsive: true,
+        mantainAspectRadio: false,
+        /* Add in the range for the Y-axis. Where I live, the temperature varies from 15-35 °C with a 5 °C buffer range, that gives us a minimus value of 10 and maximum of 40. */
+        scales: {
+            yAxes: [{
+                ticks: {
+                    suggestedMin: 10,
+                    suggestedMax: 40
+                }
+            }]
+        }
+    }
+});
+
+/* Get the contex of the humidity canvas element. */
+const humitityCanvasCtX = document.getElementById('humidity-chart').getContext('2d');
+export const humidityChart = new Chart(humitityCanvasCtX,
+{
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            data: [],
+            backgroundColor: 'rgba(255, 205, 210, 0.5)'
+        }]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        responsive: true,
+        mantainAspectRadio: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    suggestedMin: 0,
+                    suggestedMax: 100
+                }
+            }]
+        }
+    }
+});
--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+import { temperatureChart, humidityChart} from 'charts';
+
 const fetchTemperature = () => {
     /* The fetch API uses a promise based sintax.
     It may look a little weird if you are seeing it for the first time, but it's an improvement over callbacks. */
@@ -10,6 +12,8 @@ const fetchTemperature = () => {
         /* In our server API route handler, the format of returned data was an object with a 'value' property. */
         /* Get the 'p' element as a variable, and set its inner HTML to the response we received from the server. 
         The value of the sensor reading is therefore available in 'data.value' */
+        pushData(temperatureChart.data.labels, getNowTimeStamp(), 10);
+        pushData(temperatureChart.data.datasets[0].data, data.value, 10);
         const temperatureDisplay = document.getElementById('temperature-display').innerHTML = '<strong>' + data.value + '</strong>';
     });
 }
@@ -38,72 +42,6 @@ const getNowTimeStamp = () => {
     const timeNow = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
     return timeNow;
 }
-
-/* Get the contex of the temperature canvas element. */
-const temperatureCanvasCtX = document.getElementById('temperature-chart').getContext('2d');
-/* Create a new chart on the context we just instanciated. */
-const temperatureChart = new Chart(temperatureCanvasCtX,
-{
-    /* We are going to show the ongoing temperature as a line chart. */
-    type: 'line',
-    data: {
-        /* This is mock data.
-        The labels, which will form our x-axis, are suppose to represent the time at which each reading was taken.
-        Finally, we add the dataset, whose data is an array of temperature values.
-        The background color is set to the same value as earlier display, with some added transparency (which is why the 'rgba' representation is used) */
-        labels: ['10:30', '10.31', '10.32', '10.33'],
-        datasets: [{
-            data: [12, 19, 23, 17],
-            backgroundColor: 'rgba(255, 205, 210, 0.5)'
-        }]
-    },
-    options: {
-        /* There is no need for a legend since there is only one dataset plotted.
-        The 'responsive' and 'mantainAspectRadio' options are set so that the chart takes the width and height of the canvas, and does not set it on its own. */
-        legend: {
-            display: false
-        },
-        responsive: true,
-        mantainAspectRadio: false,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    suggestedMin: 10,
-                    suggestedMax: 40
-                }
-            }]
-        }
-    }
-});
-
-/* Get the contex of the humidity canvas element. */
-const humitityCanvasCtX = document.getElementById('humidity-chart').getContext('2d');
-const humidityChart = new Chart(humitityCanvasCtX,
-{
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            data: [],
-            backgroundColor: 'rgba(255, 205, 210, 0.5)'
-        }]
-    },
-    options: {
-        legend: {
-            display: false
-        },
-        responsive: true,
-        mantainAspectRadio: false,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    suggestedMin: 0,
-                    suggestedMax: 100
-                }
-            }]
-        }
-    }
-});
 
 /* Call the above defined function at regular intervals */
 setInterval(() => {
