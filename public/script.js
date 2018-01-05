30e6bbcfb613abdaf4317cf82a39e241e24c4232
Simone Boscolo Berto <simone.boscoloberto@it.ibm.com>
2018-01-05 02:10:49 +0100

    Added range and avarage readings

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 /* Import temperature and humidity chart configuration from charts module using ES6. */
 import {temperatureChart, humidityChart, temperatureChartConfig, humidityChartConfig} from './charts.js';
 
+const temperatureDisplay = document.getElementById('temperature-display');
 const fetchTemperature = () => {
     /* The fetch API uses a promise based sintax.
     It may look a little weird if you are seeing it for the first time, but it's an improvement over callbacks. */
@@ -27,7 +28,7 @@ const fetchTemperature = () => {
         /* In our server API route handler, the format of returned data was an object with a 'value' property. */
         /* Get the 'p' element as a variable, and set its inner HTML to the response we received from the server. 
         The value of the sensor reading is therefore available in 'data.value' */
-        const temperatureDisplay = document.getElementById('temperature-display').innerHTML = '<strong>' + data.value + '</strong>';
+        temperatureDisplay.innerHTML = '<strong>' + data.value + '</strong>';
     });
 }
 
@@ -77,6 +78,7 @@ const fetchTemperatureRange = () => {
 }
 
 /* Repeat the same steps for the humidity API */
+const humidityDisplay = document.getElementById('humidity-display');
 const fetchHumidity = () => {
     fetch('/humidity').then(results => {
         return results.json();
@@ -84,7 +86,7 @@ const fetchHumidity = () => {
         pushData(humidityChart.data.labels, getTimeStamp(), 10);
         pushData(humidityChart.data.datasets[0].data, data.value, 10);
         humidityChart.update();
-        const humidityDisplay = document.getElementById('humidity-display').innerHTML = '<strong>' + data.value + '</strong>';
+        humidityDisplay.innerHTML = '<strong>' + data.value + '</strong>';
     });
 }
 
