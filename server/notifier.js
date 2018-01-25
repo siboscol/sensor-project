ad188558f6ba27214c73b78789d5f00dbcce4fef
Simone Boscolo Berto <simone.boscoloberto@it.ibm.com>
2018-01-25 01:26:15 +0000

    Created notifier module and incluse socket.io on server.

---
+++ server/notifier.js
@@ -0,0 +1,46 @@
+/**
+ * Define the listeners (for temperature and humidity)
+ */
+const listeners = {
+    temperature: [],
+    humidity: []
+};
+
+/**
+ * The subscribe function takes two arguments, the callback function and the type.
+ * It pushes the provided listener to our array of listeners 
+ * (either the temperature listener array, or the humidity listener array, depending on the "type" provided).
+ */
+const subscribe = (listener, type) => {
+    listeners[type].push(listeners);
+};
+
+/**
+ * If we want to remove a listener from our list, we call the unsubscribe function.
+ * This accepts the exact types of arguments as the subscribe function.
+ */
+const unsubscribe = (removedListener, type) => {
+    /**
+     * The list of listener is sifted through and
+     * the listener functions are filtered on the basis of whether they are the same as the listener provided.
+     * Those that are the same (which would be the only one) are removed.
+     */
+    listeners[type] = listeners[type].filter(listener => listener != removedListener);
+}
+
+/**
+ * The notify function is used by the cache when it receives an update from the sensor.
+ * It takes a value and a type as argument, and notify all the listener of that type that a new value has been received.
+ */
+const notify = (value, type) => {
+    listeners[type].forEach(listener => {
+        listener(value);
+    });
+}
+
+/**
+ * The functions that are to be used by other modules are exported.
+ */
+module.exports = {
+    subscribe, unsubscribe, notify
+}
